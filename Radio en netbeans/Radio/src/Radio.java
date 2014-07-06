4f82b49eeda61cceb5352d6420735f86347b0c28
danGeren <ger13158@uvg.edu.gt>
2014-07-05 20:07:29 -0600

    Trabajo en RAdio
    No toquen la Radio

--- Radio en netbeans/Radio/src/Radio.java
+++ Radio en netbeans/Radio/src/Radio.java
@@ -8,6 +8,51 @@
  * @author Daniel
  * @author Ed. Chamo
  */
-public class Radio {
+public class Radio implements Interfaz{
+
+    private double frecuenciaAM, frecuenciaFM;
+    private boolean power, frecuencia;
+    private double[] favoritosAM, favoritosFM;
+    
+    public Radio(){
+        power=false;
+        frecuencia=false;
+        favoritosAM=new double[12];
+        favoritosFM=new double[12];
+        frecuenciaAM=530;
+        frecuenciaFM=87.9;
+    
+    }
+    
+    @Override
+    public void cambiarFrecuencia(boolean frecuencia) {
+       frecuencia=true;
+    }
+
+    @Override
+    public double sacar(int boton) {
+        //todavía falta programarlo
+        return 9;
+    }
+
+    @Override
+    public void apagar() {
+    }
+
+    @Override
+    public void encender() {
+    }
+
+    @Override
+    public void guardar(int boton, double estacion) {
+    }
+
+    @Override
+    public void subirEstacion(boolean frecuencia) {
+    }
+
+    @Override
+    public void bajarEstacion(boolean frecuencia) {
+    }
     
 }
