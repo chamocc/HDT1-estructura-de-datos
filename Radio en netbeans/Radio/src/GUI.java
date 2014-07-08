48e2284aa6db3cbff8fb4f228150b0784c7b4a76
danGeren <ger13158@uvg.edu.gt>
2014-07-07 19:14:47 -0600

    Segui con GUI

--- Radio en netbeans/Radio/src/GUI.java
+++ Radio en netbeans/Radio/src/GUI.java
@@ -31,11 +31,53 @@ public class GUI extends JPanel{
         power=new JButton("OFF");
         frecuencia=new JButton("FM");
         guardarLeer=new JButton("Guardar");
+        estacion=new JLabel ("87.9");
         
         favoritos=new JButton[12];
         for(int i=0; i<12; i++){
             favoritos[i]=new JButton("Fav #"+(i+1));
         }
-        
+        ListenerBoton listener1= new ListenerBoton();
+	bajarEstacion.addActionListener(listener1);
+        subirEstacion.addActionListener(listener1);
+        power.addActionListener(listener1);
+        frecuencia.addActionListener(listener1);
+        guardarLeer.addActionListener(listener1);
+    }
+    
+    private class ListenerBoton implements ActionListener
+	{
+            public void actionPerformed(ActionEvent event)
+            {
+                  if(event.getSource()==bajarEstacion)
+			{
+                            miRadio.bajarEstacion(miRadio.getFrecuencia());
+                            estacion.setText(miRadio.getEstacion());
+                        }
+                  if(event.getSource()==subirEstacion)
+			{
+                            miRadio.subirEstacion(miRadio.getFrecuencia());
+                            estacion.setText(miRadio.getEstacion());
+                        }
+                  if(event.getSource()==power)
+			{
+                            if(miRadio.getEncendido()){
+                                miRadio.apagar();
+                                power.setText("OFF");
+                            }else{
+                                miRadio.encender();
+                                power.setText("ON");
+                            }
+                        }
+                  
+                 if(event.getSource()==frecuencia)
+			{
+                            if(miRadio.getFrecuencia()){
+                                miRadio.cambiarFrecuencia(false);
+                            }else{
+                                miRadio.cambiarFrecuencia(true);
+                            }
+                        } 
+            }
     }
 }
