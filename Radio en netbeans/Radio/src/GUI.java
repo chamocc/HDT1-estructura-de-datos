87c1c27bf246446fecdb78eb28dbc7af2959b573
danGeren <ger13158@uvg.edu.gt>
2014-07-07 18:54:54 -0600

    GUI

---
+++ Radio en netbeans/Radio/src/GUI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Daniel
+ * @author Edgar Chamo
+ * @author Alejandro Diaz
+ */
+public class GUI extends JPanel{
+    
+    private JButton bajarEstacion, subirEstacion, power, frecuencia;
+    private JButton[] favoritos;
+    private JLabel estacion;
+    private Radio miRadio;
+    private boolean guardar;
+    
+    public GUI(){
+        
+    }
+}
