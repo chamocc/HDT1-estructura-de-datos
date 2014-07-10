0bea24f5e959c1937263fd21b277720d8ce1c559
danGeren <ger13158@uvg.edu.gt>
2014-07-09 19:47:49 -0600

    Javadoc Radio

--- Radio en netbeans/Radio/src/Radio.java
+++ Radio en netbeans/Radio/src/Radio.java
@@ -8,12 +8,19 @@
  * @author Daniel 13158
  * @author Edgar Chamo 13083
  * @author Alejandro Diaz 13082
+ * 
+ * @since 5/7/2014
  */
 public class Radio implements Interfaz{
 
     private double frecuenciaAM, frecuenciaFM;
     private boolean power, frecuencia;
     private double[] favoritosAM, favoritosFM;
+    
+    /**
+     * Este metodo es el constructro de la clase
+     * No recibe nada y no devuelve nada
+     */
     
     public Radio(){
         power=true;
@@ -24,12 +31,23 @@ public class Radio implements Interfaz{
         frecuenciaFM=87.9;
     
     }
+    /**
+     * Este metodo cambia la frecuencia del radio segun lo que se mande
+     * No devuelve nada
+     * @param _frecuencia es true para FM y false para AM
+     */
     
     @Override
     public void cambiarFrecuencia(boolean _frecuencia) {
        frecuencia=_frecuencia;
     }
 
+    
+    /**
+     * Este metodo obtiene el valor guardado en un espacio de la lista de favoritos
+     * @param boton es el boton donde esta guardado el valor
+     * @return devuelve la frecuencia tipo double
+     */
     @Override
     public double sacar(int boton) {
         double val;
@@ -43,16 +61,29 @@ public class Radio implements Interfaz{
         return val;
     }
 
+    /**
+     * Este metodo pone en false el atributo power de la clase
+     * No devuelve nada
+     */
     @Override
     public void apagar() {
         power=false;
     }
 
+    /**
+     * Este metodo pone en true el atributo power de la clase
+     * No devuelve nada
+     */
     @Override
     public void encender() {
         power=true;
     }
 
+    /**
+     * Este metodo guarda el valor actual de la estacion en alguno de los botones
+     * @param boton indica en que botón se guarda
+     * @param estacion indica que estación se debe guardar
+     */
     @Override
     public void guardar(int boton, double estacion) {
          if(frecuencia){
@@ -62,6 +93,10 @@ public class Radio implements Interfaz{
         }
     }
 
+    /**
+     * Aumenta el valor de la estación. 
+     * @param frecuencia Si la frecuencia es true lo hace para FM y si es false lo hace para AM
+     */
     @Override
     public void subirEstacion(boolean frecuencia) {
          if(frecuencia){
@@ -76,7 +111,10 @@ public class Radio implements Interfaz{
             }
         }
     }
-
+     /**
+     * Disminuye el valor de la estación. 
+     * @param frecuencia Si la frecuencia es true lo hace para FM y si es false lo hace para AM
+     */
     @Override
     public void bajarEstacion(boolean frecuencia) {
         if(frecuencia){
@@ -91,21 +129,35 @@ public class Radio implements Interfaz{
             }
         }
     }
-    
+    /**
+     * Devuelve true si power es true y false si power es false
+     * @return devuelve boolean
+     */
     @Override
     public boolean getEncendido() {
         return power;
     }
     
+    /**
+     * Obtiene la frecuencia AM
+     * @return devuelve double
+     */
     @Override
     public double getEstacionAM(){
         return frecuenciaAM;
     }
-    
+    /**
+     * Obtiene la frecuencia FM
+     * @return devuelve double
+     */
     public double getEstacionFM(){
         return frecuenciaFM;
     }
     
+    /**
+     * Devuelve true si frecuencia es true y false si power es false
+     * @return devuelve boolean
+     */
     @Override
     public boolean getFrecuencia(){
         return frecuencia;
