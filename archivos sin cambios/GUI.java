0e4490ce9b79a8a2cde48c6fe652916e893e30ed
chamocc <chamocc@gmail.com>
2014-07-11 07:56:34 -0600

    guarde los programas originales en otra carpeta

---
+++ archivos sin cambios/GUI.java
@@ -0,0 +1,206 @@
+/*
+ *Esta clase es la parte grafica del programa
+ *la cual le permite al usuario interactuar con los botones
+ *simulando una radio de la vida real. 
+ */
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Daniel Gerendas 13158
+ * @author Edgar Chamo 13083
+ * @author Alejandro Diaz 13082
+ * 
+ * @since 5/07/2014
+ */
+public class GUI extends JPanel{
+    
+    private JButton bajarEstacion, subirEstacion, power, frecuencia, guardarLeer;
+    private JButton[] favoritos;
+    private JLabel estacion;
+    private InterfazRadio miRadio;
+    private boolean guardar;
+    private Color c1;
+   
+    //metodos
+    //constructor: se encarga de inicializar las varibles que se utilizaran en esta clase, asi como de las caracteristicas del panel
+    public GUI(){
+        
+        
+        c1= new Color(224,20,15);
+        setBackground(c1);
+
+        setLayout(null);
+
+        miRadio=new Radio();
+        guardar=true;
+        bajarEstacion=new JButton("-");
+        bajarEstacion.setBounds(10, 140, 50, 50);
+        bajarEstacion.setBackground(Color.ORANGE);
+        
+        subirEstacion=new JButton("+");
+        subirEstacion.setBounds(10, 80, 50,50 );
+        subirEstacion.setBackground(Color.ORANGE);
+        
+        power=new JButton("ON");
+        power.setBounds(10, 10, 60,60 );
+        power.setBackground(Color.ORANGE);
+        
+        frecuencia=new JButton("AM");
+        frecuencia.setBounds(80, 10, 80, 50);
+        frecuencia.setBackground(Color.ORANGE);
+        
+        guardarLeer=new JButton("Guardar");
+        guardarLeer.setBounds(170, 10, 80,50 );
+        guardarLeer.setBackground(Color.ORANGE);
+                
+        estacion=new JLabel ("530");
+        estacion.setBounds(80, 70, 50,50 );
+        estacion.setBackground(Color.ORANGE);
+        
+        favoritos=new JButton[12];
+        for(int i=0; i<12; i++){
+            favoritos[i]=new JButton("Fav #"+(i+1));
+            favoritos[i].setBounds(10+(i*90),250,90,50);
+            favoritos[i].setBackground(Color.ORANGE);
+        }
+
+        ListenerBoton listener1= new ListenerBoton();
+	bajarEstacion.addActionListener(listener1);
+        subirEstacion.addActionListener(listener1);
+        power.addActionListener(listener1);
+        frecuencia.addActionListener(listener1);
+        guardarLeer.addActionListener(listener1);
+        
+        for(int i=0; i<12; i++){
+            favoritos[i].addActionListener(listener1);
+        }
+        
+        add(bajarEstacion);
+        add(subirEstacion);
+        add(estacion);
+        add(power);
+        add(frecuencia);
+        add(guardarLeer);
+        for(int i=0; i<12; i++){
+            add(favoritos[i]);
+        }
+        setPreferredSize(new Dimension(1200,300));
+    }
+    
+    private class ListenerBoton implements ActionListener
+	{
+            public void actionPerformed(ActionEvent event)
+            {
+                  if(event.getSource()==bajarEstacion)
+			{
+                            boolean val=miRadio.getFrecuencia();
+                            miRadio.bajarEstacion();
+                            if(miRadio.getFrecuencia()){
+                                estacion.setText(miRadio.getEstacionFM()+"");
+                            }else{
+                                estacion.setText(miRadio.getEstacionAM()+"");
+                            }
+                            
+                        }
+                  if(event.getSource()==subirEstacion)
+			{
+                            boolean val=miRadio.getFrecuencia();
+                            miRadio.subirEstacion();
+                           if(miRadio.getFrecuencia()){
+                                estacion.setText(miRadio.getEstacionFM()+"");
+                            }else{
+                                estacion.setText(miRadio.getEstacionAM()+"");
+                            }
+                        }
+                  if(event.getSource()==power)
+			{
+                            if(miRadio.getEncendido()){
+                                miRadio.apagar();
+                                power.setText("OFF");
+                                apagado();
+                            }else{
+                                miRadio.encender();
+                                power.setText("ON");
+                                encendido();
+                            }
+                        }
+                  
+                 if(event.getSource()==frecuencia)
+			{
+                            if(miRadio.getFrecuencia()){
+                                miRadio.cambiarFrecuencia(false);
+                                frecuencia.setText("AM");
+                            }else{
+                                miRadio.cambiarFrecuencia(true);
+                                frecuencia.setText("FM");
+                            }
+                        }
+                 
+                  if(event.getSource()==guardarLeer)
+			{
+                            if(guardar){
+                                guardar=false;
+                                guardarLeer.setText("Leer");
+                            }else{
+                                guardar=true;
+                                guardarLeer.setText("Guardar");
+                            }
+                        }
+                  
+                 for(int i=0; i<12; i++){
+                     if(event.getSource()==favoritos[i]){
+                         if(guardar){
+                             double val=0;
+                             if(miRadio.getFrecuencia()){
+                               val=miRadio.getEstacionFM();
+                            }else{
+                               val=miRadio.getEstacionAM();
+                            }
+                             miRadio.guardar(i, val);
+                         }else{
+                             estacion.setText(miRadio.sacar(i)+"");
+                         }
+                     } 
+                 }
+            }
+
+    }
+    
+    /** 
+     * Encendido: este metodo se encarga de habilitar los botones dentro del panel
+     */
+     public void encendido()
+     {
+        bajarEstacion.setEnabled(true);
+        subirEstacion.setEnabled(true);
+        frecuencia.setEnabled(true);
+        guardarLeer.setEnabled(true);
+        for (int i =0;i<12;i++ )
+        {
+            favoritos[i].setEnabled(true);
+        }
+     }
+     
+     /**
+      * Apagado este metodo se encarga de deshabilitar los botnes dentro del panl
+      */
+     public void apagado()
+     {
+        bajarEstacion.setEnabled(false);
+        subirEstacion.setEnabled(false);
+        frecuencia.setEnabled(false);
+        guardarLeer.setEnabled(false);
+        for (int i =0;i<12;i++ )
+        {
+            favoritos[i].setEnabled(false);
+        }
+     }
+    
+    
+    
+}
---
+++ archivos sin cambios/Radio.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Daniel 13158
+ * @author Edgar Chamo 13083
+ * @author Alejandro Diaz 13082
+ * 
+ * @since 5/7/2014
+ */
+public class Radio implements InterfazRadio{
+
+    private double frecuenciaAM, frecuenciaFM;
+    private boolean power, frecuencia;
+    private double[] favoritosAM, favoritosFM;
+    
+    /**
+     * Este metodo es el constructro de la clase
+     * No recibe nada y no devuelve nada
+     */
+    
+    public Radio(){
+        power=true;
+        frecuencia=false;
+        favoritosAM=new double[]{530,530,530,530,530,530,530,530,530,530,530,530};
+        favoritosFM=new double[]{87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9};
+        frecuenciaAM=530;
+        frecuenciaFM=87.9;
+    
+    }
+    /**
+     * Este metodo cambia la frecuencia del radio segun lo que se mande
+     * No devuelve nada
+     * @param _frecuencia es true para FM y false para AM
+     */
+    
+    @Override
+    public void cambiarFrecuencia(boolean _frecuencia) {
+       frecuencia=_frecuencia;
+    }
+
+    
+    /**
+     * Este metodo obtiene el valor guardado en un espacio de la lista de favoritos
+     * @param boton es el boton donde esta guardado el valor
+     * @return devuelve la frecuencia tipo double
+     */
+    @Override
+    public double sacar(int boton) {
+        double val;
+        if(frecuencia){
+            val=favoritosFM[boton];
+            frecuenciaFM=val;
+        }else{
+            val=favoritosAM[boton];
+            frecuenciaAM=val;
+        }
+        return val;
+    }
+
+    /**
+     * Este metodo pone en false el atributo power de la clase
+     * No devuelve nada
+     */
+    @Override
+    public void apagar() {
+        power=false;
+    }
+
+    /**
+     * Este metodo pone en true el atributo power de la clase
+     * No devuelve nada
+     */
+    @Override
+    public void encender() {
+        power=true;
+    }
+
+    /**
+     * Este metodo guarda el valor actual de la estacion en alguno de los botones
+     * @param boton indica en que botón se guarda
+     * @param estacion indica que estación se debe guardar
+     */
+    @Override
+    public void guardar(int boton, double estacion) {
+         if(frecuencia){
+            favoritosFM[boton]=estacion;
+        }else{
+            favoritosAM[boton]=estacion;
+        }
+    }
+
+    /**
+     * Aumenta el valor de la estación. 
+     * @param frecuencia Si la frecuencia es true lo hace para FM y si es false lo hace para AM
+     */
+    @Override
+    public void subirEstacion() {
+         if(frecuencia){
+            frecuenciaFM+=0.2;
+            if(frecuenciaFM>107.9){
+                frecuenciaFM=87.9;
+            }
+        }else{
+            frecuenciaAM+=10;
+            if(frecuenciaAM>1610){
+                frecuenciaAM=530;
+            }
+        }
+    }
+     /**
+     * Disminuye el valor de la estación. 
+     * @param frecuencia Si la frecuencia es true lo hace para FM y si es false lo hace para AM
+     */
+    @Override
+    public void bajarEstacion() {
+        if(frecuencia){
+            frecuenciaFM-=0.2;
+            if(frecuenciaFM<87.9){
+                frecuenciaFM=107.9;
+            }
+        }else{
+            frecuenciaAM-=10;
+            if(frecuenciaAM<530){
+                frecuenciaAM=1610;
+            }
+        }
+    }
+    /**
+     * Devuelve true si power es true y false si power es false
+     * @return devuelve boolean
+     */
+    @Override
+    public boolean getEncendido() {
+        return power;
+    }
+    
+    /**
+     * Obtiene la frecuencia AM
+     * @return devuelve double
+     */
+    @Override
+    public double getEstacionAM(){
+        return frecuenciaAM;
+    }
+    /**
+     * Obtiene la frecuencia FM
+     * @return devuelve double
+     */
+    public double getEstacionFM(){
+        return frecuenciaFM;
+    }
+    
+    /**
+     * Devuelve true si frecuencia es true y false si power es false
+     * @return devuelve boolean
+     */
+    @Override
+    public boolean getFrecuencia(){
+        return frecuencia;
+    }
+    
+    
+}
